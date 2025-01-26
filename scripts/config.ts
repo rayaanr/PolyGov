97ccbbb29624795a79a05aa6f7e05c73703e9034
rayaanr <rayaannsn@gmail.com>
2025-01-27 00:30:31 +0530

    refactor: remove Lock module and related tests; update .gitignore to exclude .env files

--- scripts/config.ts
+++
@@ -1,6 +0,0 @@
-import { NetworkConfig } from "./deploy/types";
-
-export const config: NetworkConfig = {
-  arb: { contractAddress: "0x662C260c8Bd205A0427C4B18a3F30fA27c775210" },
-  bsc: { contractAddress: "0x31397A82ecd7CF98365D417530FbeE74476A5106" }
-};
