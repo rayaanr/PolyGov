ea864235c98421ab642e06a3d08420b17041245a
rayaanr <rayaannsn@gmail.com>
2025-01-27 01:27:38 +0530

    refactor: remove old deployment scripts and related types for BSC and Arbitrum

--- scripts/deploy/deploy-bsc.ts
+++
@@ -1,8 +0,0 @@
-import { deployAndVerify } from "./deploy";
-import "dotenv/config";
-
-async function main() {
-  await deployAndVerify("GovernanceBSC", [], "bscTestnet");
-}
-
-main().catch(console.error);
--- scripts/deploy/types.ts
+++
@@ -1,5 +0,0 @@
-export interface NetworkConfig {
-    [key: string]: {
-        contractAddress: string;
-    };
-}
